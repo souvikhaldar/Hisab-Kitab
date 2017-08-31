44e2429a520214d947fe1d9d075ec66db14c7450
souvikhaldar <souvikhaldar32@gmail.com>
2017-09-01 02:50:11 +0530

    login added

--- app/src/server.py
+++ app/src/server.py
@@ -91,7 +91,7 @@ def login():
         except Exception as e:
             print(e)
             flash("Invalid Credentials","warning")
-			return redirect(url_for('about'))
+            return redirect(url_for('login'))
 
 
 
