9d8430182ae7d6b28abbacd4b6c2f1de4d91a67e
souvikhaldar <souvikhaldar32@gmail.com>
2017-09-01 01:42:42 +0530

    auth done

--- app/src/server.py
+++ app/src/server.py
@@ -9,7 +9,56 @@ from wtforms import Form,StringField,TextAreaField,PasswordField,validators,Inte
 #from passlib.hash import sha256_crypt
 from functools import wraps
 
+#check if logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args,**kwargs):
+        if 'logged_in' in session:
+            return f(*args,**kwargs)
+        else:
+            flash('Unauthorized, please log in','danger')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route("/")
 def index():
     return render_template("home.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+class RegisterForm(Form):
+	name=StringField('Name',[validators.Length(min=1,max=30)])
+	username=StringField('Username',[validators.Length(min=5,max=100)])
+	email=StringField('Email',[validators.Length(min=5,max=50)])
+	password=PasswordField('Password',[
+		validators.DataRequired(),
+		validators.EqualTo('confirm',message='Passwords do not match')
+		])
+	confirm=PasswordField('Confirm Password')
+@app.route('/register',methods=['GET','POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method=='POST' and form.validate():
+        name=form.name.data
+        email=form.email.data
+        username=form.username.data
+        password=form.password.data
+        url = 'http://auth.hasura/'
+        data = {'username': username, 'password': password}
+        headers = {'Content-Type' : 'application/json'}
+
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+
+        return json.dumps(r.json(), indent=4)
+        #return r.text
+        '''
+        mysqldb things:-
+        cur=mysql.connection.cursor()
+        cur.execute("insert into users(name,email,username,password) values (%s, %s, %s, %s)",(name,email,username,password))
+        mysql.connection.commit()
+        cur.close()'''
+        flash('You are now registered','success')
+        return redirect(url_for('index'))
+    return render_template('register.html',form=form)
