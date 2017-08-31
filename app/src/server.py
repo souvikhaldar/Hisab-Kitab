52d0362c167a5b3773ea16f30ac07a0d5bdf1800
souvikhaldar <souvikhaldar32@gmail.com>
2017-09-01 02:06:39 +0530

    auth done

--- app/src/server.py
+++ app/src/server.py
@@ -51,7 +51,7 @@ def register():
 
         r = requests.post(url, data=json.dumps(data), headers=headers)
 
-        return json.dumps(r.json(), indent=4)
+        #return json.dumps(r.json(), indent=4)
         #return r['auth_token']
         '''
         mysqldb things:-
