78ad255c5e00129722320e501dc7ff631f3f1d6a
souvikhaldar <souvikhaldar32@gmail.com>
2017-09-01 01:22:38 +0530

    home added

--- app/src/server.py
+++ app/src/server.py
@@ -2,8 +2,14 @@ from src import app
 from flask import jsonify, request
 import requests
 import json
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
+#from flask_mysqldb import MySQL
+#from data import Articles
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators,IntegerField
+#from passlib.hash import sha256_crypt
+from functools import wraps
 
 
 @app.route("/")
-def hello():
-    return json.dumps({"message":"Hello World!"})
+def index():
+    return render_template("home.html")
