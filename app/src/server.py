a6f0a33187a8ee60c9301557329a42e386aaa8d6
souvikhaldar <souvikhaldar32@gmail.com>
2017-09-02 00:23:56 +0530

    Dashboard added with errors

--- app/src/server.py
+++ app/src/server.py
@@ -115,6 +115,15 @@ def dashboard():
         print('The type is ',r)
 
         articles=r.json()
+
+        #user info
+        url = 'http://auth.hasura/user/account/info'
+        headers = {'Content-Type' : 'application/json','X-Hasura-User-Id': '1','X-Hasura-Role': 'admin'}
+        d = requests.post(url, headers=headers)
+        print(d)
+        f=d.json()
+        print("data",f)
+        print(f['username'])
         return render_template('dashboard.html',articles=articles)
     except Exception as e:
         print(e)
